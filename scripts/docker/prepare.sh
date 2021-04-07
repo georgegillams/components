9f4148c5e9816a53d3f19c6729ba75c778bbc5ef
George Gillams <george.gillams@skyscanner.net>
2021-04-07 19:30:48 +0100

    [NO-JIRA] Quiten prepare script

--- scripts/docker/prepare.sh
+++ scripts/docker/prepare.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # build image if it doesn't already exist
-if !(docker images | grep gg-snapshot-test); then
-echo "building"
+if !(docker images | grep gg-snapshot-test > /dev/null); then
     docker build -t gg-snapshot-test -f Dockerfile .
 fi
 
 # create container if it doesn't already exist
-if !(docker ps -a | grep gg-snapshot-test); then
+if !(docker ps -a | grep gg-snapshot-test > /dev/null); then
     docker create -t gg-snapshot-test bash
 fi
 
