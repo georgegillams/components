7955ed32b75a4a3961aed881ace844423d604c4b
George Gillams <code@georgegillams.co.uk>
2022-06-19 13:51:52 +0100

    feat: Add medal shelf

---
+++ src/medal-shelf/index.js
@@ -0,0 +1,1 @@
+export { default } from './medal-shelf';
---
+++ src/medal-shelf/medal-shelf.stories.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import EventPatch from '../event-patch';
+
+import MedalShelf from './index';
+
+export default {
+  title: 'Styled/Molecules/Medal shelf',
+  component: MedalShelf,
+};
+
+export const Default = () => (
+  <MedalShelf>
+    <EventPatch
+      background="red"
+      foreground="yellow"
+      title="Event name"
+      year="1941"
+      stravaLink="https://strava.com"
+    />
+    <EventPatch
+      background="red"
+      foreground="yellow"
+      title="Event name"
+      year="1941"
+      stravaLink="https://strava.com"
+    />
+    <EventPatch
+      background="red"
+      foreground="yellow"
+      title="Event name"
+      year="1941"
+      stravaLink="https://strava.com"
+    />
+    <EventPatch
+      background="red"
+      foreground="yellow"
+      title="Event name"
+      year="1941"
+      stravaLink="https://strava.com"
+    />
+    <EventPatch
+      background="red"
+      foreground="yellow"
+      title="Event name"
+      year="1941"
+      stravaLink="https://strava.com"
+    />
+  </MedalShelf>
+);
