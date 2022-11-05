d5543c450a77514786920daec077b11ac25c8509
George Gillams <code@georgegillams.co.uk>
2022-11-05 08:07:22 +0000

    feat: Effect hoc improvements (#649)

--- src/effects/index.js
+++ src/effects/index.js
@@ -1,2 +1,3 @@
 export { withScrollAnimation } from './with-scroll-animation';
 export { ScrollAnimationWrapper } from './scroll-animation-wrapper';
+export { ANIMATIONS } from '../animated-content';
