4bd4e73ddd65b5aed2cdf8542e2e9326809bc1d4
George Gillams <code@georgegillams.co.uk>
2022-11-20 18:02:17 +0000

    fix: Revert modularisation (#666)

--- scripts/release.js
+++ scripts/release.js
@@ -25,7 +25,7 @@ const updatePackageFile = (newVersion) => {
   newPackageData.version = newVersion;
   const fileContent = `${JSON.stringify(newPackageData, null, 2)}\n`;
   writeFileSync('package.json', fileContent, 'utf8');
-  execSync('yarn transpile:package-json');
+  execSync('cp package.json ./dist/');
   execSync('cp README.md ./dist/');
   console.log(blue('package.json updated'));
 };
