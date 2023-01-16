f9710a4938c7307232efeb7be19e094a226698cc
George Gillams <code@georgegillams.co.uk>
2023-01-16 22:23:16 +0000

    Update main.cjs (#688)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*stories.js'],
+  stories: ['../src/**/*stories.js', '../src/**/*stories.tsx'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-actions',
